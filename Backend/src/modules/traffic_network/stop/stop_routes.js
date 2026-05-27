575d425a3637ee5612d3b0e0328fc7400fe20290
Amparo <amparoNatividad.Mendoza@alu.uclm.es>
2026-05-27 13:42:12 +0200

    Se termino la parte publica en un desarrollo anterio, mas precisamente en el commit bd58ef5, por lo tanto la parte de que el usuario pueda ver las paradas y el tiempo aproximado de cada parada esta cubierto (#49)

--- Backend/src/modules/traffic_network/stop/stop_routes.js
+++ Backend/src/modules/traffic_network/stop/stop_routes.js
@@ -7,6 +7,8 @@ const stopController = require('./stop_controller.js');
 
 const router = express.Router();
 
+//Publico
+
 // Rutas públicas — cualquiera puede ver paradas
 router.get('/',          stopController.getAllStop);
 router.get('/:id_stop',  stopController.getStop);
@@ -17,6 +19,7 @@ router.get('/route/:id_route',               stopController.getStopsByRoute);
 // Tiempo estimado de llegada — pública
 router.get('/:id_stop/arrival/:id_route',    stopController.getArrivalTime);
 
+//Privado
 // Gestión de paradas — solo empresa
 router.post('/',         authMiddleware, roleMiddleware('company'), validate(addStopRules),    stopController.addStop);
 router.put('/:id_stop',  authMiddleware, roleMiddleware('company'), validate(updateStopRules), stopController.updateStop);
